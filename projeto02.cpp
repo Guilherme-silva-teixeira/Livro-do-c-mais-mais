99141ece2e8f53cc7fd5d053fa52d33dddc10287
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-03-01 17:41:31 -0300

    Update projeto02.cpp

--- projeto02.cpp
+++ projeto02.cpp
@@ -5,13 +5,24 @@ using namespace std;
 int main(void)
 {
 	string option;
+	int number;
+	string res;
 	cin >> option;
 	if (option == "y")
 	{
 		//o programa roda aqui
+		while (1)
+		{
+			cout << "adivinhe um número\n";
+		}
 	}
 	else if (option == "n")
 	{
 		//o programa irá terminar
+	}
+	else
+	{
+		//o código se encerra
+		cout << "erro (opção inválida)";
 	}
 }
