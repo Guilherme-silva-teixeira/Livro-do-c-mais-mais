88c2b88d071a7d396a78f6dc06fdc4c5a4d1f376
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-03-01 17:55:33 -0300

    Update projeto02.cpp

--- projeto02.cpp
+++ projeto02.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
 int main(void)
 {
+	setlocale(LC_ALL, "portuguese");
 	string option;
 	int number;
-	string res;
+	int res;
+	cout << "Digite 'y' para jogar e 'n' para sair\n";
 	cin >> option;
 	if (option == "y")
 	{
 		//o programa roda aqui
-		while (1)
-		{
 			cout << "adivinhe um número\n";
-		}
+			cout << "o número deve ser um número entre 0 e 1000\n";
+			number = rand() % 1001;
+			while (1)
+			{
+				cout << "? ";
+				cin >> res;
+				if (res < number)
+				{
+					cout << "O número é um número maior do que esse.\nTente denovo:\n";
+				}else if (res > number)
+				{
+					cout << "O número é um número menor do que esse.\nTente denovo:\n";
+				}else if (res == number)
+				{
+					cout << "Parabéns! você acertou";
+					return 1;
+				}
+			}
 	}
 	else if (option == "n")
 	{
