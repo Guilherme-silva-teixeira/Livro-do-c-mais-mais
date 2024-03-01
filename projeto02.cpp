efde63036d99c75b2b000c970c7ce3a9e3bb9cb9
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-03-01 17:38:06 -0300

    Create projeto02.cpp

---
+++ projeto02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string option;
+	cin >> option;
+	if (option == "y")
+	{
+		//o programa roda aqui
+	}
+	else if (option == "n")
+	{
+		//o programa irá terminar
+	}
+}
